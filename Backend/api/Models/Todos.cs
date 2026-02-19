391e192d0f1ef83da8f2eff453240417f5599ed0
Manuel <manuelmoisa10@gmail.com>
2026-02-19 13:09:56 +0100

    Modell v2

--- Backend/api/Models/Todos.cs
+++ Backend/api/Models/Todos.cs
@@ -2,8 +2,8 @@
 public class Todos
 {
     public int Id { get; set; }
-    public string Text { get; set; }
+    public string Text { get; set; } = string.Empty;
     public bool Is_done { get; set; }
-    public DateTime Kreiert { get; set; }
-    public DateTime Bearbeitet { get; set; }
+    public DateTime Kreiert { get; set; } = DateTime.Now;
+    public DateTime Bearbeitet { get; set; } = DateTime.Now;
 }
